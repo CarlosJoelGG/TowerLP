c666e344e4490fd49aa22dea1b5575b6adc843d0
CarlosJoelGG <carlosjoelgg@gmail.com>
2022-09-03 13:45:30 -0400

    parche Sonido
    no te jodas

---
+++ Assets/AddCasa.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCasa : MonoBehaviour
+{
+    public List<GameObject> casas,tokens;
+    public GameObject Data;
+    public GameObject prefa;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+    public void Iniciar()
+    {
+        destruir();
+        Data = GameObject.Find("mundo");
+        List<GameObject> aux = new List<GameObject>();
+        aux = Data.GetComponent<ubicarmundo>().objetosdelmundo;
+        tokens = new List<GameObject>();
+        for (int i = 0; i < aux.Count; i++)
+        {
+            if (aux[i].GetComponent<StateInf>().id > 1)
+            {
+                casas.Add(aux[i]);
+            }
+        }
+        for (int i = 0; i < casas.Count; i++)
+        {
+            prefa.GetComponent<gestion>().Data = casas[i];
+            tokens.Add(Instantiate(prefa));
+            tokens[i].transform.parent = transform;
+            tokens[i].transform.position = new Vector3(0, 0, 0);
+            tokens[i].transform.localScale = new Vector3(1, 1, 1);
+            tokens[i].transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+    public void destruir()
+    {
+        for (int i = 0; i < tokens.Count; i++)
+        {
+            Destroy(tokens[i]);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/CheckboxConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckboxConfiguration : MonoBehaviour
+{
+    public Sprite vacio, Relleno;
+    public Image imagen;
+    public musica data;
+    public bool onoff = true;
+    public int funcion = 0;
+    public Slider efectos, musica;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if(efectos!=null)
+        efectos.onValueChanged.AddListener(data.volumenEfectos);
+        if(musica!=null)
+        musica.onValueChanged.AddListener(data.volumenMusica);
+    }
+    void Start()
+    {
+        data = GameObject.Find("musica").GetComponent<musica>();
+    }
+    public void inicio()
+    {
+        switch (funcion)
+        {
+            case 0:
+                data.muteMusic();
+                break;
+            case 1:
+                data.muteSFX();
+                break;
+        }
+        if (onoff)
+        {
+            onoff = false;
+            imagen.sprite = vacio;
+        }
+        else
+        {
+            onoff = true;
+            imagen.sprite = Relleno;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
